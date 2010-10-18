34476d131a7c43bce3adce86557085d049a62e29
Matthew Sackman <matthew@rabbitmq.com>
2010-10-18 01:00:39 +0100

    Simplify package.mk due to improvements to the way in which variables are assigned their default values

--- package.mk
+++ package.mk
@@ -8,9 +8,7 @@ ERLTL_DIR:=$(PACKAGE_DIR)/erltl
 ERLTL_EBIN_DIR:=$(ERLTL_DIR)/ebin
 ERLTL_SRC_DIR:=$(ERLTL_DIR)/src
 
-EBIN_DIR:=$(PACKAGE_DIR)/ebin
-SOURCE_DIR:=$(PACKAGE_DIR)/src
-EBIN_BEAMS:=$$(patsubst $$($(PACKAGE_DIR)_SOURCE_DIR)/%.erl,$$($(PACKAGE_DIR)_EBIN_DIR)/%.beam,$$($(PACKAGE_DIR)_SOURCE_ERLS)) $$($(PACKAGE_DIR)_EBIN_DIR)/template.beam
+EBIN_BEAMS:=$(patsubst $(SOURCE_DIR)/%.erl,$(EBIN_DIR)/%.beam,$(SOURCE_ERLS)) $(EBIN_DIR)/template.beam
 
 $(ERLTL_EBIN_DIR)/%.beam: $(ERLTL_SRC_DIR)/%.erl
 	@mkdir -p $(ERLTL_EBIN_DIR)
@@ -19,5 +17,5 @@ $(ERLTL_EBIN_DIR)/%.beam: $(ERLTL_SRC_DIR)/%.erl
 $(EBIN_DIR)/template.beam: $(SOURCE_DIR)/template.et $(ERLTL_EBIN_DIR)/erltl.beam
 	$(ERL) -I -pa $(ERLTL_EBIN_DIR) -noshell -eval 'erltl:compile("$<", [{outdir, "$(@D)"}, report_errors, report_warnings, nowarn_unused_vars]), halt().'
 
-clean::
+$(PACKAGE_DIR)/clean::
 	rm -f $(ERLTL_EBIN_DIR)/*.beam
